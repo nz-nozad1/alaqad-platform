d0db6b4130322cde271ca716f481931f3c5fbd9f
nz-nozad1 <nz.nozad7@gmail.com>
2026-08-31 23:43:50 +0300

    Fix React Router base path

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import Home from './pages/Home'
 
 function App() {
   return (
-    <BrowserRouter>
+    <BrowserRouter basename="/alaqad-platform">
       <Routes>
         <Route path="/" element={<Home />} />
       </Routes>
